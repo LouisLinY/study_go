f6c57e7373f9eb07b0aef6d9c3c2c37a534aa687
LouisLinY <Louislin15@yahoo.com>
2018-08-23 00:31:18 +0800

    go 数组学习

---
+++ study_data_structure_array/study_array01.go
@@ -0,0 +1,14 @@
+// study_list01.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//定义数组方法
+	var arr1 [5]int
+	arr2 := [3]int{1, 3, 5}
+	arr3 := [...]int{2, 4, 6, 8, 10}
+	fmt.Println(arr1, arr2, arr3)
+}
